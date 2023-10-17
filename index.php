974308ee6fe19c740b1fe00085e048253d5a4dc6
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-10-16 20:43:00 -0500

    prueba de creacion de tabla

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
-    $dbhost = "webintphp-server.mysql.database.azure.com";
-    $dbuser = "ypxsdmnyef";
-    $dbpassword = "Y5BJJ5XACVJDLZZK$";
-    $dbname = "webintphp-database";
-    $port = 3306;
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
-    if (!$conn){
-        die("No hay conexión: ".mysqli_connect_error());
+
+    $servername = "webintphp-server.mysql.database.azure.com";
+    $username = "ypxsdmnyef";
+    $password = "Y5BJJ5XACVJDLZZK$";
+    $database = "webintphp-database";
+
+    // Crear una conexión
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
     }
+
+    $sql = "CREATE TABLE tareas (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(50) NOT NULL,
+        descripcion VARCHAR(100) NOT NULL
+    )";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Tabla creada con éxito";
+    } else {
+        echo "Error al crear la tabla: " . $conn->error;
+    }
+
+    $conn->close();
 
     echo "Hello db";
 ?>
