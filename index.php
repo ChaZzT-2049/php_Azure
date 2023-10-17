a34a193ead18ea78aac28427f55e27fcaf1caadd
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-10-16 20:21:50 -0500

    conexion a base de datos azure

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Hello World";
+    $dbhost = "webintphp-server.mysql.database.azure.com";
+    $dbuser = "ypxsdmnyef";
+    $dbpassword = "Y5BJJ5XACVJDLZZK$";
+    $dbname = "webintphp-database";
+    $port = 3306;
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $port);
+    if (!$conn){
+        die("No hay conexión: ".mysqli_connect_error());
+    }
+
+    echo($conn)
 ?>
